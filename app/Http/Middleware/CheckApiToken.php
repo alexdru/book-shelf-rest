f0cd96b2b3cbc2ec701d3c0f0a9a812b0d458454
alexdru <alexdru@outlook.com>
2021-09-21 22:50:36 +0500

    add middleware for checking api token

---
+++ app/Http/Middleware/CheckApiToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->token) {
+            return response()->json('Unauthorized', 401);
+        }
+
+        return $next($request);
+    }
+}
